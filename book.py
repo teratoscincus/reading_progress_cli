711323fb8a4f4c64c3a57a91e6882f9ed94e3a30
Max <teratoscincus@hotmail.com>
2022-09-29 19:12:54 +0200

    Create Book class
    Has title, currently_reading, current_page, and chapters attributes.
    The new_book() method assigns values to the title and chapters
    attributes.

---
+++ book.py
@@ -0,0 +1,55 @@
+class Book:
+    """A representation of a book."""
+
+    def __init__(self):
+        """
+        Init attributes:
+        title as an empty string.
+        currently_reading as False.
+        current_page as an integer value of 1.
+        chapters as an empty dict.
+        """
+        self.title = ""
+        self.currently_reading = False
+        self.current_page = 1
+        self.chapters = {}
+
+    def new_book(self):
+        """Set values for the title and chapters attribute."""
+        self._set_book_info()
+
+    def _set_book_info(self):
+        """Set the values of the title and chapters attribute."""
+        self.title = input("Title of book: ").lower()
+
+        print("Enter 'q' when there are no more chapters to add.")
+        chapters = {}
+        while True:
+            chapter_name = input("Enter name of chapter: ").lower()
+            if chapter_name == "q":
+                break
+
+            # First page is 1 for the first chapter.
+            if len(self.chapters) == 0:
+                chapter_first_page = 1
+            else:
+                chapter_first_page = chapter_last_page + 1
+
+            chapter_last_page = self._set_page_number()
+            chapter_total_pages = chapter_last_page - chapter_first_page
+
+            self.chapters[chapter_name] = {
+                "finished": False,
+                "first_page": chapter_first_page,
+                "last_page": chapter_last_page,
+                "total_pages": chapter_total_pages,
+            }
+
+    def _set_page_number(self):
+        """Set page number and check that it's a numerical value."""
+        while True:
+            page_number = input("Last page of chapter: ")
+            try:
+                return int(page_number)
+            except ValueError:
+                print("Must be a numerical value. Try again.")
