75121b9ed8812a4982e24dbff7b68cd3325ff66c
Max <teratoscincus@hotmail.com>
2022-10-04 14:53:21 +0200

    Implement feature to mark chapter as finished

--- reading_progress.py
+++ reading_progress.py
@@ -10,8 +10,8 @@ parser.add_argument(
     nargs="?",
     const=True,
     help=(
-        "Add a new book to the collection. "
-        "Contents of the book will be filled in through the CLI."
+        "Add a new book to the collection."
+        " Contents of the book will be filled in through the CLI."
     ),
 )
 # Change book marked as currently reading.
@@ -19,8 +19,8 @@ parser.add_argument(
     "-cr",
     "--currently_reading",
     help=(
-        "Switch between books to mark as currently reading. "
-        "Specify the title of the book withing quotation marks."
+        "Switch between books to mark as currently reading."
+        " Specify the title of the book withing quotation marks."
     ),
 )
 # Mark a page as current page.
@@ -28,8 +28,8 @@ parser.add_argument(
     "-bp",
     "--bookmark_page",
     help=(
-        "Bookmark current page of book marked as currently reading. "
-        "Accepts an integer."
+        "Bookmark current page of book marked as currently reading."
+        " Accepts an integer."
     ),
     type=int,
 )
@@ -38,8 +38,11 @@ parser.add_argument(
     "-fc",
     "--finished_chapter",
     help=(
-        "Log a chapter in the book marked as currently reading as finished. "
-        "Specify the name of the chapter withing quotation marks."
+        "Log a chapter in the book marked as currently reading as finished."
+        " Specify the name of the chapter withing quotation marks."
+        " Checks if bookmarked page is in the chapter being marked as finished."
+        " If so, the bookmark will be moved to the first page of the first chapter"
+        " identified as unfinished."
     ),
 )
 args = parser.parse_args()
@@ -57,7 +60,19 @@ if args.currently_reading:
 
 if args.bookmark_page:
     # Update value of current_page attribute.
-    currently_read_book.current_page = args.bookmark_page
+    currently_read_book.bookmark_page(args.bookmark_page)
 
-    # Save changes.
-    library.archive_book(currently_read_book)
+if args.finished_chapter:
+    chapter = currently_read_book.chapters[args.finished_chapter]
+
+    # Mark given chapter as finished.
+    chapter["finished"] = True
+
+    # Make sure current page is not in a chapter marked as finished.
+    current_page = currently_read_book.current_page
+    # Attempt to bookmark current_page.
+    currently_read_book.bookmark_page(current_page)
+
+
+# Save changes.
+library.archive_book(currently_read_book)
