3853250c38271ff226013704bbb95ae5382527d3
Max <teratoscincus@hotmail.com>
2022-10-05 15:21:17 +0200

    Edit error messages

--- library.py
+++ library.py
@@ -36,7 +36,8 @@ class Library:
 
     def set_currently_reading_book(self, book_title):
         """
-        Checkout book and mark it as currently being read.
+        Mark book specified by title as currently being read, and return an instance of
+        the book currently being read.
         Only one book at a time will be marked as currently being read.
         Removes status of currently being read from previous book that had it set to
         True.
@@ -45,17 +46,15 @@ class Library:
             # Remove status of previously read book.
             previously_read_book = self.get_currently_read_book()
             previously_read_book.currently_reading = False
+
             self.archive_book(previously_read_book)
 
             # Change status of book specifies by title.
             currently_read_book = self._get_book_by_title(book_title)
             currently_read_book.currently_reading = True
-            self.archive_book(currently_read_book)
-        else:
-            print(
-                f"Sorry, couldn't find '{book_title}' in the collection."
-                "\nAre you sure you spelled the title correctly?"
-            )
+
+            # Return an instance of the book.
+            return currently_read_book
 
     def _get_book_by_title(self, book_title):
         """Return an instance of a book specified by its title."""
--- reading_progress.py
+++ reading_progress.py
@@ -76,7 +76,20 @@ if args.add_book:
 if args.currently_reading:
     book_title = args.currently_reading.strip().lower()
     # Mark book as currently being read.
-    library.set_currently_reading_book(book_title)
+    currently_read_book = library.set_currently_reading_book(book_title)
+
+    # Check if given book title is in the library's collection.
+    if currently_read_book:
+        # Save changes.
+        library.archive_book(currently_read_book)
+    else:
+        print(
+            f"\n  Sorry, couldn't find '{book_title}' in the collection.\n"
+            "  Are you sure you spelled the title correctly?\n"
+        )
+        print("  Books currently in the collection:")
+        for book_title in library.collection:
+            print(f"    · {book_title.title()}")
 
 # Init book marked as currently being read.
 currently_read_book = library.get_currently_read_book()
@@ -85,6 +98,9 @@ currently_read_book = library.get_currently_read_book()
 if args.bookmark_page:
     # Update value of current_page attribute.
     currently_read_book.bookmark_page(args.bookmark_page)
+
+    # Save changes.
+    library.archive_book(currently_read_book)
 
 # Mark a chapter in book currently being read as completed.
 if args.finished_chapter:
@@ -100,17 +116,37 @@ if args.finished_chapter:
         current_page = currently_read_book.current_page
         # Attempt to bookmark current_page.
         currently_read_book.bookmark_page(current_page)
+
+        # Save changes.
+        library.archive_book(currently_read_book)
+
+        # Used for below call to calculate_progress() and print().
+        currently_read_chapter = currently_read_book.get_current_chapter_name()
+        currently_finished_chapters = currently_read_book.get_finished_chapter_names()
+
+        # Parse calculate_progress() call argument.
+        currently_read_book_info = (
+            currently_read_book,
+            currently_finished_chapters,
+            currently_read_chapter,
+        )
+        # Calculate reading progress.
+        progress_info = calculate_progress(currently_read_book_info)
+
+        # Print reading progress.
+        print_progress(currently_read_book_info, progress_info)
     else:
         warning_message = (
-            f"'{chapter_title.title()}' is not a chapter of"
-            f" '{currently_read_book.title.title()}'.\n"
-            "Perhaps there was an typo in the given title, or it's part of another"
-            " book?"
+            f"\n  Sorry, couldn't find the chapter '{chapter_title.title()}' in the"
+            " book.\n"
+            "  Perhaps there was an typo in the given title, or it's part of another"
+            " book?\n"
         )
         print(warning_message)
-
-# Save changes.
-library.archive_book(currently_read_book)
+        # List chapters of currently read book.
+        print(f"  Chapters of '{currently_read_book.title.title()}':")
+        for chapter in currently_read_book.chapters:
+            print(f"    · {chapter.title()}")
 
 # List chapters of currently read book.
 if args.list_chapters:
@@ -126,7 +162,7 @@ elif args.list_books:
         print(f"    · {book_title.title()}")
 # Show reading progress of book currently being read.
 # Only do so when no listing or adding of a new book is made to reduce CLI clutter.
-elif not args.add_book:
+elif not (args.add_book or args.finished_chapter or args.currently_reading):
     # Used for below call to calculate_progress() and print().
     currently_read_chapter = currently_read_book.get_current_chapter_name()
     currently_finished_chapters = currently_read_book.get_finished_chapter_names()
